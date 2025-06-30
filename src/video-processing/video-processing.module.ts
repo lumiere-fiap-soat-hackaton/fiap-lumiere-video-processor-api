6462a95df918c49bd115eb44aad44b1230187434
Alexandre Maeda <ale.b.maeda@gmail.com>
2025-06-29 23:23:06 -0300

    feat: implement VideoProcessingModule with DynamoDB integration

--- src/video-processing/video-processing.module.ts
+++ src/video-processing/video-processing.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
-import { VideoProcessingsController } from '@video-processing/api/controllers/video-processing.controller';
+import { VideoProcessingsController } from './api/controllers/video-processing.controller';
+import { DynamoDbModule } from './infrastructure/database/dynamodb/dynamodb.module';
+import { DynamoDbVideoRepository } from './infrastructure/repositories/dynamodb-video.repository';
+import { VIDEO_REPOSITORY } from './domain/repositories/video.repository';
 
 @Module({
-  imports: [],
-  providers: [],
+  imports: [DynamoDbModule],
+  providers: [
+    {
+      provide: VIDEO_REPOSITORY,
+      useClass: DynamoDbVideoRepository,
+    },
+  ],
   controllers: [VideoProcessingsController],
+  exports: [VIDEO_REPOSITORY],
 })
 export class VideoProcessingModule {}
