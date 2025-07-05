1d33ec3659339759ab5511dad130f85fd52b7d73
Alexandre Maeda <ale.b.maeda@gmail.com>
2025-07-05 11:17:44 -0300

    feat: update VideoRepository.create method to include 'status' in video parameters

--- src/video-processing/domain/repositories/video.repository.ts
+++ src/video-processing/domain/repositories/video.repository.ts
@@ -3,7 +3,9 @@ import { Video } from '../entities/video.entity';
 export const VIDEO_REPOSITORY = 'VIDEO_REPOSITORY';
 
 export interface VideoRepository {
-  create(video: Omit<Video, 'id' | 'createdAt' | 'updatedAt'>): Promise<Video>;
+  create(
+    video: Omit<Video, 'id' | 'createdAt' | 'updatedAt' | 'status'>,
+  ): Promise<Video>;
   findById(id: string): Promise<Video | null>;
   findAll(): Promise<Video[]>;
   update(id: string, data: Partial<Video>): Promise<Video>;
